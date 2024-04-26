4bd4cf34dbf333dbbab3d856a32ff9cd6d706f9a
Shashank Surjekar <shashanksurjekar@gmail.com>
2024-04-26 17:17:08 +0530

    Forgot password page done

--- app/src/App.jsx
+++ app/src/App.jsx
@@ -7,6 +7,7 @@ import Signup from "./pages/Auth/Signup/signup";
 import Signin from "./pages/Auth/Signin/Signin";
 import RegisterEmailVerify from "./pages/Auth/RegisterEmailVerify/RegisterEmailVerify";
 import RegisterSuccess from "./pages/Auth/RegisterSuccess/RegisterSuccess";
+import ForgotPassword from "./pages/Auth/ForgotPassword/ForgotPassword";
 
 const router = createBrowserRouter([
   {
@@ -36,6 +37,9 @@ const router = createBrowserRouter([
   {
     path: "/register-success",
     element: <RegisterSuccess />,
+  },{
+    path: "/forgot-password",
+    element: <ForgotPassword />,
   },
 ]);
 
---
+++ app/src/pages/Auth/ForgotPassword/ForgotPassword.jsx
@@ -0,0 +1,78 @@
+import {
+  Box,
+  Center,
+  Icon,
+  VStack,
+  Button,
+  Checkbox,
+  FormControl,
+  FormErrorMessage,
+  FormLabel,
+  HStack,
+  Input,
+  Stack,
+  Text,
+} from "@chakra-ui/react";
+import Card from "../../../components/Card";
+import { FaArrowLeftLong } from "react-icons/fa6";
+import { Link } from "react-router-dom";
+import { Formik, Form, Field } from "formik";
+import { object, string, ref } from "yup";
+
+const ForgotPassword = () => {
+  const forgotValidationSchema = object({
+    email: string().email("Email is Invalid").required("Email is required"),
+  });
+  return (
+    <Center minH="100vh">
+      <Card>
+        <Link to="/signin">
+          <Icon as={FaArrowLeftLong} boxSize="6" />
+        </Link>
+        <Text mt="4" fontWeight="medium" textStyle="h1">
+          Forgot Password
+        </Text>
+        <Text textStyle="p2" color="black.60" mt="4">
+          Enter your email address for which account you want to reset your
+          password.
+        </Text>
+        <Formik
+          initialValues={{
+            email: "",
+            password: "",
+          }}
+          onSubmit={(values) => {
+            console.log(values);
+          }}
+          validationSchema={forgotValidationSchema}
+        >
+          {() => (
+            <Form>
+              <Stack mt={8} spacing={6}>
+                <Field name="email">
+                  {({ field, meta }) => (
+                    <FormControl isInvalid={!!(meta.error && meta.touched)}>
+                      <FormLabel htmlFor="email">Email</FormLabel>
+                      <Input
+                        {...field}
+                        name="email"
+                        type="email"
+                        placeholder="Enter Your Email..."
+                      />
+                      <FormErrorMessage>{meta.error}</FormErrorMessage>
+                    </FormControl>
+                  )}
+                </Field>
+                <Button variant="outline" w="full">
+                  Reset Account
+                </Button>
+              </Stack>
+            </Form>
+          )}
+        </Formik>
+      </Card>
+    </Center>
+  );
+};
+
+export default ForgotPassword;
