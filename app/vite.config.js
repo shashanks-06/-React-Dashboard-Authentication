cc4c6259cf1cb4fada7af0ed2cc82acf5af3d77a
Shashank Surjekar <shashanksurjekar@gmail.com>
2024-04-30 16:28:36 +0530

    Deployment

--- app/vite.config.js
+++ app/vite.config.js
@@ -4,5 +4,6 @@ import pluginRewriteAll from "vite-plugin-rewrite-all";
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: "/React-Dashboard-Authentication/",
   plugins: [react(), pluginRewriteAll()],
 });
